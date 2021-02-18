d7cb70cb81109e0d7c37585739c4a3926ead5cbb
cristobalcastrillon <44101193+cristobalcastrillon@users.noreply.github.com>
2021-02-18 15:39:59 -0500

    Añadí la línea de ejemplo que había borrado en el anterior commit.

--- src/co/edu/javeriana/calculatorgit/Authors.java
+++ src/co/edu/javeriana/calculatorgit/Authors.java
@@ -3,5 +3,6 @@ package co.edu.javeriana.calculatorgit;
 public class Authors {
 	public static String CAPA = "Carlos Parra";
 	public static String CCB = "Cristóbal Castrillón Balcázar";
+	public static String AA = "Another Author";
 
 }
