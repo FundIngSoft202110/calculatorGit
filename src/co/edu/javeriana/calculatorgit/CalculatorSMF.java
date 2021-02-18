badb4b70a09037d0a81c98f048e5ccfbd024dbe2
Sebastian <smolanof@javeriana.edu.co>
2021-02-18 15:47:20 -0500

    Agrego implementación de calculadora Sebastian Molano Fragale

---
+++ src/co/edu/javeriana/calculatorgit/CalculatorSMF.java
@@ -0,0 +1,32 @@
+package co.edu.javeriana.calculatorgit;
+
+/**
+ * @author Sebastián Molano
+ */
+public class CalculatorSMF implements ICalculator {
+
+    @Override
+    public double add(double a, double b) {
+        return a+b;
+    }
+
+    @Override
+    public double substract(double a, double b) {
+        return a-b;
+    }
+
+    @Override
+    public double multiply(double a, double b) {
+        return a*b;
+    }
+
+    @Override
+    public double divide(double a, double b) throws ArithmeticException {
+        return a/b;
+    }
+
+    @Override
+    public String about() {
+        return "Calculadora simple que contiene las 4 operaciones básicas de una calculadora. Autor: Sebastián Molano Fragale";
+    }
+}
