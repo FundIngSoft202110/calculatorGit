117572bab9621fa232e32088676945d2389e8460
Fabian Rojas <fabian_rojas-10@hotmail.com>
2021-02-19 15:37:26 -0500

    Agregada la Calculadora de FARM

--- src/co/edu/javeriana/calculatorgit/Authors.java
+++ src/co/edu/javeriana/calculatorgit/Authors.java
@@ -5,5 +5,5 @@ public class Authors {
 	public static String CCB = "Cristóbal Castrillón Balcázar";
 	public static String AA = "Another Author";
 	public static String SMF = "Sebastián Molano Fragale";
-
+	public static String FARM = "Fabian Andres Rojas Molina";
 }
