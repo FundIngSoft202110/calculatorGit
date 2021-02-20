dcc0f2b10dc70f51e318aec5334a3c85e30e806a
richfon <richfonpan@gmail.com>
2021-02-20 00:09:33 -0500

    Termina CalculatorRFT

--- src/co/edu/javeriana/calculatorgit/CalculatorRFT.java
+++ src/co/edu/javeriana/calculatorgit/CalculatorRFT.java
@@ -4,20 +4,26 @@ public class CalculatorRFT implements ICalculator {
 
 	@Override
 	public double add(double a, double b) {
-		// TODO Auto-generated method stub
-		return 0;
+		return (a+b);
 	}
 
 	@Override
 	public double substract(double a, double b) {
-		// TODO Auto-generated method stub
-		return 0;
+		return (a-b);
 	}
 
 	@Override
 	public double multiply(double a, double b) {
-		// TODO Auto-generated method stub
-		return 0;
+		return (a*b);
 	}
+	@Override
+	public double divide (double a, double b) throws ArithmeticException{
+		return(a/b);
+	}
+	@Override
+	public String about() {
+		return("Calculator implementend by"+Authors.RFT);
+	}
+
 
 }
