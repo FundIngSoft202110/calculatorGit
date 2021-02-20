3646cca04898e17e884b08acdf4d40d42847fab4
Julian Tarazona <jdtarazona@javeriana.edu.co>
2021-02-19 20:48:09 -0500

    Se agrego autor JDTT

--- src/co/edu/javeriana/calculatorgit/Authors.java
+++ src/co/edu/javeriana/calculatorgit/Authors.java
@@ -5,5 +5,6 @@ public class Authors {
 	public static String CCB = "Cristóbal Castrillón Balcázar";
 	public static String AA = "Another Author";
 	public static String SMF = "Sebastián Molano Fragale";
+	public static String JDTT = "Julian David Tarazona Torres";
 
 }
