39dcf90728f607b26569e515b9a031153ac53ee0
Sebastian Barreto <jsebastian.barretoj99@gmail.com>
2021-02-22 16:22:35 -0500

    Se agrego excepcion en Calculator JSBJ

--- src/co/edu/javeriana/calculatorgit/CalculatorJSBJ.java
+++ src/co/edu/javeriana/calculatorgit/CalculatorJSBJ.java
@@ -20,9 +20,12 @@ public class CalculatorJSBJ implements ICalculator{
 
     @Override
 	public double divide(double a, double b) throws ArithmeticException{
+        if(b == 0){
+            throw new ArithmeticException("El divisor no puede ser 0");
+        }
         return a/b;
     }// divide
-
+    
 	@Override
 	public String about(){
         return "Calculator implemented by " + Authors.JSBJ;
