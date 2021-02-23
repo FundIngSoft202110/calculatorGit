7985b7e4190495097ed7b97ee7c8b9a5f6dc67b3
jpablo9988 <65927907+jpablo9988@users.noreply.github.com>
2021-02-23 12:17:52 -0500

    Update Authors.java

--- src/co/edu/javeriana/calculatorgit/Authors.java
+++ src/co/edu/javeriana/calculatorgit/Authors.java
@@ -3,6 +3,7 @@ package co.edu.javeriana.calculatorgit;
 public class Authors {
 	public static String CAPA = "Carlos Parra";
 	public static String CCB = "Cristóbal Castrillón Balcázar";
+	public static String JPA = "Juan Pablo Amorocho";
 	public static String AA = "Another Author";
 	public static String SMF = "Sebastián Molano Fragale";
 	public static String FARM = "Fabian Andres Rojas Molina";
