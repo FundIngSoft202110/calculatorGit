98571925554d2fae1846dde5dd1f8cdccc2a86bb
sdsuarez <71355692+sdsuarez@users.noreply.github.com>
2021-02-23 08:47:19 -0500

    Create CalculadoraDSSB.java

---
+++ src/co/edu/javeriana/calculatorgit/CalculadoraDSSB.java
@@ -0,0 +1,34 @@
+package co.edu.javeriana.calculatorgit;
+
+/**
+ * @author David Suárez
+ */
+
+public class CalculatorDNCS implements ICalculator {
+	
+    @Override
+    public double add(double a, double b) {
+        return a + b;
+    }
+
+    @Override
+    public double substract(double a, double b) {
+        return a - b;
+    }
+
+    @Override
+    public double multiply(double a, double b) {
+        return a * b;
+    }
+
+    @Override
+    public double divide(double a, double b) throws ArithmeticException {
+        return a / b;
+    }
+
+    @Override
+    public String about() {
+        return "Calculadora con las cuatro operaciones básicas " + Authors.DSSB;
+    }
+
+}
