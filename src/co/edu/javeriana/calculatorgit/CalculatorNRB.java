c298cab8beab5646864216d818f79aa077b80ced
Nicolas Ramos <nicolasramos98@gmail.com>
2021-02-23 11:26:47 -0500

    x

---
+++ src/co/edu/javeriana/calculatorgit/CalculatorNRB.java
@@ -0,0 +1,36 @@
+package co.edu.javeriana.calculatorgit;
+
+/**
+ * @author Nicolas Ramos Bohórquez
+ */
+
+public class CalculadoraSGY implements ICalculator{
+
+  @Override
+  public double add(double a, double b){
+    
+    return a + b;
+  }
+
+  @Override
+  public double substract (double a, double b){
+    
+    return a - b;
+  }
+
+  @Override
+  public double multiply (double a, double b){
+    
+    return a * b;
+  }
+
+  @Override
+  public double divide (double a, double b) throws ArithmeticException{
+    return a / b;
+  }
+
+  @Override	
+  public String about(){
+    return "Calculator implemented by " + Authors.SGY;
+  }
+}
