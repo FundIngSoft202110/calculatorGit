080e471c50b02df904df8f863b23e29db935cb7f
Daniel Millan <78319864+millandaniel@users.noreply.github.com>
2021-02-23 18:29:05 -0500

    Update Authors.java

--- src/co/edu/javeriana/calculatorgit/Authors.java
+++ src/co/edu/javeriana/calculatorgit/Authors.java
@@ -2,14 +2,14 @@ package co.edu.javeriana.calculatorgit;
 
 public class Authors {
 	public static String CAPA = "Carlos Parra";
-	public static String CCB = "Cristóbal Castrillón Balcázar";
+	public static String CCB = "CristÃ³bal CastrillÃ³n BalcÃ¡zar";
 	public static String JPA = "Juan Pablo Amorocho";
 	public static String AA = "Another Author";
-	public static String SMF = "Sebastián Molano Fragale";
+	public static String SMF = "SebastiÃ¡n Molano Fragale";
 	public static String FARM = "Fabian Andres Rojas Molina";
-	public static String SSD = "Sebastian Sandoval Dueñas";
+	public static String SSD = "Sebastian Sandoval DueÃ±as";
 	public static String JDTT = "Julian David Tarazona Torres";
-	public static String RFT ="Richard Fonseca Triviño";
+	public static String RFT ="Richard Fonseca TriviÃ±o";
 	public static String DJMG = "Daniel Jose Monsalve Guaracao";
 	public static String MARR="Manuel Alejandro Rios Romero";
 	public static String SGY = "Diana Stefania Garcia Yomayuza";
@@ -20,19 +20,21 @@ public class Authors {
 	public static String JADL = "Juan Alejadnro Diaz Lote";
 	public static String DADC = "David Andres Duarte Clavijo";
 	public static String JJBM = "Juan Jose Bolanos Melo";
-	public static String MAVD = "Marco Antonio Valencia Dueñas";
+	public static String MAVD = "Marco Antonio Valencia DueÃ±as";
 	public static String DNCS = "Diana Natalia Chaparro Sanabria";
 	public static String ARPE = "Andres Ricardo Porras Escobar";
 	public static String MJNR = "Maria Jose Nino Rodriguez";
 	public static String DSQE = "David Santiago Quintana Echavarria";
 	public static String PAS = "Pablo Santander Alvarez";
     public static String JCH = "Janet Chen He";
-	public static String DSSB ="David Santiago Suárez Barragán";
-	public static String NRB ="Nicolas Ramos Bohórquez";
+	public static String DSSB ="David Santiago SuÃ¡rez BarragÃ¡n";
+	public static String NRB ="Nicolas Ramos BohÃ³rquez";
 	public static String IRRC = "Ivan Rene Ramirez Castro";
-	public static String MRG = "Mariana Rojas Galav�s";
+	public static String MRG = "Mariana Rojas Galavís";
 	public static String BFSG = "Bryan Felipe Santillan Gomez";
 	public static String EHPS = "Esteban Hernando Pedraza Solano";
 	public static String JGOC = "Jerson Giovany Osorio Cely"
+	public static String DAMP = "Daniel Alejandro Millan Peña"
+
 
 }
