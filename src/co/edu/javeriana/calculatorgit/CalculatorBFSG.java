fb3bcb1672ea4c58286617cd96646434957b1a00
Bryan Felipe Santillan Gomez <santillan-bryan@javeriana.edu.co>
2021-02-23 13:10:53 -0500

    Create Calculadora BFSG

---
+++ src/co/edu/javeriana/calculatorgit/CalculatorBFSG.java
@@ -0,0 +1,27 @@
+public class CalculatorFARM implements ICalculator {
+
+    @Override
+    public double add(double a, double b) {
+        return a + b;
+    }
+
+    @Override
+    public double substract(double a, double b) {
+        return a - b;
+    }
+
+    @Override
+    public double multiply(double a, double b) {
+        return a * b;
+    }
+
+    @Override
+    public double divide(double a, double b) throws ArithmeticException {
+        return a / b ;
+    }
+
+    @Override
+    public String about() {
+        return "Calculadora implementada por: " + Authors.BFSG;
+    }
+}
